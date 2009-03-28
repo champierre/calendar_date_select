042ce0ecec1be5edfdeb77efff6f8cfead6e0b5f
Junya Ishihara <junya@champierre.com>
2009-03-29 01:44:38 +0900

    refactoring

--- public/javascripts/calendar_date_select/format_japanese.js
+++ public/javascripts/calendar_date_select/format_japanese.js
@@ -9,7 +9,7 @@ Date.prototype.toFormattedString = function(include_time){
 }
 
 Date.parseFormattedString = function (string) {
-  var regexp = "([0-9]{4}\/([0-1]?[0-9])\/[0-3]?[0-9]) ([0-9]{2}:[0-9]{2})";
+  var regexp = "([0-9]{4}\/[0-1]?[0-9]\/[0-3]?[0-9]) ([0-9]{2}:[0-9]{2})";
 
   var d = string.match(new RegExp(regexp, "i"));
   if (d==null) {
@@ -19,9 +19,9 @@ Date.parseFormattedString = function (string) {
   ymd = d[1].split('/');
   hrs = 0;
   mts = 0;
-  if(d[3] != null) {
-    hrs = parseInt(d[3].split(':')[0], 10);
-    mts = parseInt(d[3].split(':')[1], 10);
+  if(d[2] != null) {
+    hrs = parseInt(d[2].split(':')[0], 10);
+    mts = parseInt(d[2].split(':')[1], 10);
   }
   return new Date(ymd[0], parseInt(ymd[1], 10)-1, ymd[2], hrs, mts, 0);
 }
